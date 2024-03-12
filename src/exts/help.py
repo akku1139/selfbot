c8bc9fe55140f5b4f4cc829e6a15cf76969d757b
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-12 15:19:10 +0000

    囲ってあげる

--- src/exts/help.py
+++ src/exts/help.py
@@ -17,10 +17,11 @@ class Help(commands.HelpCommand):
     mapping: typing.Mapping[typing.Optional[commands.Cog], list[commands.Command]]
   ):
     await self.get_destination().send(
+      "```"+
       "Usage:	^command [option] ...\n"+
       # "Commands:\n"+cmds+"\n"
       "License AGPLv3+: GNU AGPL version 3 or later <https://www.gnu.org/licenses/agpl-3.0.html>.\n"+
-      "Source Code: https://github.com/akku1139/selfbot\n"
+      "Source Code: https://github.com/akku1139/selfbot\n```"
     )
 
 async def setup(bot: commands.Bot):
