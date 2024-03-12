cc4cbecdc7ed3da536be4a53bc5bbeb83d13d6e7
-akku- <151517253+akku1139@users.noreply.github.com>
2024-03-12 15:02:44 +0000

    ヘルプ?

--- src/exts/help.py
+++ src/exts/help.py
@@ -3,27 +3,18 @@ from selfcord.ext import commands
 import logging
 log = logging.getLogger(__name__)
 
-class HelpCog(commands.Cog, name = __name__):
-  def __init__(self, bot: commands.Bot):
-    self.bot = bot
-
-  @commands.Cog.listener()
-  async def on_ready(self):
-    log.info("loaded")
-
-  @commands.command(description='{"level":50}')
-  async def help(self, ctx: commands.Context):
-    cmds = ""
-    for cmd in self.bot.commands:
-      if cmd.hidden:
-        continue
-      cmds += f"{cmd.name} "
-      await ctx.reply(
-      "Usage:	^command [option] ...\n"+
-      "Commands:\n"+cmds+"\n"
-      "License AGPLv3+: GNU AGPL version 3 or later <https://www.gnu.org/licenses/agpl-3.0.html>.\n"+
-      "Source Code: https://github.com/akku1139/selfbot\n"
+class Help(commands.HelpCommand):
+  def __init__(self):
+    super().__init__(
+      show_hidden=False,
+      command_attrs={
+        "brief": "show this help",
+        "description": '{"level":50}',
+      }
     )
 
 async def setup(bot: commands.Bot):
-  await bot.add_cog(HelpCog(bot))
+  bot.help_command = Help()
+
+async def teardown(bot: commands.Bot):
+  bot.help_command = None
